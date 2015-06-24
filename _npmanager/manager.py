4568d1e7da9c8788fd80c8d10bc265f49dca024a
ssut <ssut@ssut.me>
2015-06-24 23:08:03 +0900

    Separate: between code and messages

--- _npmanager/manager.py
+++ _npmanager/manager.py
@@ -1,16 +1,53 @@
 """
 npmanager manager module
 """
+from __future__ import print_function
 import configparser
+import importlib
+import inspect
+import sys
+import os
+
+from _npmanager.utils import commandutils as cmdutils
+from _npmanager import messages
 
 class Manager:
     def __init__(self, conf='/etc/npmanager.conf'):
+        self.conf = conf
         self.check_env()
+        self.packages = {}
+        self._load_packages()
+
+    def _exit(self, stat):
+        sys.exit(stat)
+
+    def _load_packages(self):
+        root = '_npmanager.packages'
+        packages = importlib.import_module(root)
+        packages = inspect.getmembers(packages, inspect.ismodule)
+        for name, package in packages:
+            module = '{}.{}'.format(root, name)
+            cls, _ = inspect.getmembers(package,
+                lambda member: member.__module__ == member and \
+                               inspect.isclass(member))
+            print(cls)
 
     def check_env(self):
-        pass
+        # check for root user
+        euid = os.geteuid()
+        # check for package manager (apt)
+        aptitude = cmdutils.which('/usr/bin/apt-get') and \
+                   cmdutils.which('/usr/bin-aptitude')
+        if euid != 0:
+            print(messages.FATAL.NOT_ROOT)
+            self._exit(1)
+        if not aptitude:
+            print(messages.WARN.FOR_DEBIAN)
 
     def install(self):
-        pass
+        pyapt = cmdutils.which('/usr/bin/add-apt-repository')
+        if not pyapt:
+            print(messages.INFO.INSTALL_PYAPT)
+
 
 
---
+++ _npmanager/messages.py
@@ -0,0 +1,17 @@
+# pylint: disable=C0103,R0903,W0232,C1001,C0111
+"""
+messages for npmanager
+"""
+
+class FATAL:
+    NOT_ROOT = 'Important: this script must be run as root.'
+
+class WARN:
+    FOR_DEBIAN = 'Warning: this script was made for use on debian-based linux.'
+
+class INFO:
+    INSTALL_PYAPT = ('Info: the command "add-apt-repository" does not exist on your server.\n'
+                     '      this installation will install it on your system.')
+
+
+
