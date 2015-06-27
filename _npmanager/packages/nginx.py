66a21be644cc4ddd77539e5a1049d1d88b7f07dd
ssut <ssut@ssut.me>
2015-06-27 22:14:00 +0900

    Add: nginx and php repo definition

--- _npmanager/packages/nginx.py
+++ _npmanager/packages/nginx.py
@@ -1,4 +1,19 @@
 from _npmanager.classes import Package
 
+
 class NginxPackage(Package):
-    pass 
+    COMMAND = ''
+    SELECT = {
+        'title': 'Choose the version of NGINX',
+        'subtitle': 'WARNING: Do not use development releases on production systems!',
+        'options': [
+            {'title': 'NGINX Stable', 'repo': 'ppa:nginx/stable'},
+            {'title': 'NGINX Development', 'repo': 'ppa:nginx/development'}
+        ]
+    }
+
+    def select(self):
+        val = super(NginxPackage, self).select()
+        repo = self.SELECT.get('options')[val].get('repo')
+        self.COMMAND = 'add-apt-repository {} -y'.format(repo)
+
--- _npmanager/packages/php.py
+++ _npmanager/packages/php.py
@@ -1,4 +1,18 @@
 from _npmanager.classes import Package
 
 class PHPPackage(Package):
-    pass
+    COMMAND = ''
+    SELECT = {
+        'title': 'Choose the version of PHP',
+        'subtitle': 'WARNING: Some PHP applications may not be supported by the latest version of PHP.',
+        'options': [
+            {'title': 'PHP 5.6 (by ondrej)', 'repo': 'ppa:ondrej/php5-5.6'},
+            {'title': 'PHP 5.5 (by ondrej)', 'repo': 'ppa:ondrej/php5'}
+        ]
+    }
+
+    def select(self):
+        val = super(PHPPackage, self).select()
+        repo = self.SELECT.get('options')[val].get('repo')
+        self.COMMAND = 'add-apt-repository {} -y'.format(repo)
+
