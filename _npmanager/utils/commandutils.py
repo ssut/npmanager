05aa8122b4b8e761d46ad19ad2fb852ea0ab6fec
ssut <ssut@ssut.me>
2015-06-27 19:48:46 +0900

    Add: a function that determines terminal size

--- _npmanager/utils/commandutils.py
+++ _npmanager/utils/commandutils.py
@@ -2,6 +2,7 @@
 command utils for npmanager
 """
 import os
+import subprocess
 
 def which(program):
     """
@@ -25,4 +26,18 @@ def which(program):
                 return exe_file
 
     return None
+
+def termsize():
+    """
+    Determine current terminal size
+
+    This code written as suggested by http://stackoverflow.com/a/25730885
+    """
+    width, height = 60, 0
+    with open('/dev/tty') as tty:
+        size = subprocess.check_output(['stty', 'size'], stdin=tty).split()
+        height, width = map(int, size)
+
+    return (width, height, )
+
 
