1644a6e17a4350c187adb47b8486e758de3e3643
ssut <ssut@ssut.me>
2015-06-27 20:04:56 +0900

    Add: a package implement `line_receiver`

--- _npmanager/packages/pyapt.py
+++ _npmanager/packages/pyapt.py
@@ -7,5 +7,6 @@ from _npmanager.classes import Package
 class PyAptPackage(Package):
     COMMAND = 'apt-get install software-properties-common python-software-properties'
 
-    def install(self):
-        self.call()
+    def line_receiver(self, line):
+        pass
+
